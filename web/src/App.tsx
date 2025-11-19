0393ba42c9653792c45aec497b268deeb5380170
leon <leon@leondeMacBook-Pro.local>
2025-11-19 14:07:41 +0800

    add web env

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
 
 function App() {
+  const apiUrl = import.meta.env.VITE_API_URL
 
   return (
     <>
       <div className="text-xl text-muted-foreground text-center mt-10">welcome to workflow web.</div>
+      <div className="text-xl text-muted-foreground text-center mt-10">
+        {apiUrl || "error: none"}
+      </div>
     </>
   )
 }
